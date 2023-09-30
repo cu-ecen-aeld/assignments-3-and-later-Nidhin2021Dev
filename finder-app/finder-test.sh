4ee532ffbaae9774c16318440f296502034e5443
Nidhin2021Dev <myofficialskknidhin@gmail.com>
2023-09-30 18:16:26 +0530

    mod

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -64,7 +64,7 @@ do
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
-echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
+
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
@@ -72,6 +72,7 @@ set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
 	echo "success"
+	echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 	exit 0
 else
 	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
