2c15f96b209bb147132bab00b15986ed2efb4593
Nidhin2021Dev <myofficialskknidhin@gmail.com>
2023-10-21 18:39:56 +0530

    Assignment-5, Part-2. adding script

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DAEMON="/usr/bin/aesdsocket"
+DAEMON_ARGS="-d"
+
+case "$1" in
+	start)
+			echo "Starting aesdsocket"
+			start-stop-daemon -S -n aesdsocket --exec "$DAEMON" -- "$DAEMON_ARGS"
+			;;
+	stop)
+			echo "Stopping aesdsocket"
+			start-stop-daemon -K -n aesdsocket
+			;;
+	*)
+			echo "Usage: $0 {start|stop}"
+			exit 1
+esac
+
+exit 0
